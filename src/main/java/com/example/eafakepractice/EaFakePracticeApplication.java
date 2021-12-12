b77f9b8144bb9a3e95889077d89f6e5b2aa5ed39
agegithub <91578956+agegithub@users.noreply.github.com>
2021-12-11 22:28:11 -0600

    first commit

---
+++ src/main/java/com/example/eafakepractice/EaFakePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.eafakepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaFakePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EaFakePracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eafakepractice/EaFakePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eafakepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaFakePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
